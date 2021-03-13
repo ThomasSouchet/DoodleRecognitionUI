f99f58c97f617c1db70eb4a8a6dde5e86ead193f
Thomas Souchet <thomas.souchet@gmail.com>
2021-03-13 20:04:32 +0100

    Modification for multi-models

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import base64
 import time
 import json
 from pylab import array
+import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ExifTags
 
 st.set_page_config(layout="wide")
@@ -35,8 +36,7 @@ st.write("# Doodle Recognition: Let’s play Pictionary !")
 st.text("")
 
 # enter here the address of your api
-#url = 'https://doodle-recognition-api-x4dwwwxida-ew.a.run.app/predict/'
-url = 'http://127.0.0.1:8000/predict/'
+url = 'https://doodle-recognition-api-x4dwwwxida-ew.a.run.app/predict/'
 
 st.write("Real-time Doodle Recognition with Quick, Draw !")
 with st.beta_expander("Click here for more info about the model"):
@@ -49,6 +49,26 @@ with st.beta_expander("Click here for more info about the model"):
     """, unsafe_allow_html=True)
 
 st.text("")
+
+model = st.radio('Select a model', ('20K_NCLass_345', '25K_NCLass_190', '45K_NCLass_150', '120K_NCLass_50'))
+
+st.write(model)
+
+model_name = ''
+num_class = 0
+
+if model == '20K_NCLass_345':
+    model_name = 'models_model_V1_DPoint_20K_NCLass_345.h5'
+    num_class = 345
+elif model == '25K_NCLass_190':
+    model_name = 'models_model_V1_DPoint_25K_NCLass_190.h5'
+    num_class = 190
+elif model == '45K_NCLass_150':
+    model_name = 'models_model_V1_DPoint_45K_NCLass_150.h5'
+    num_class = 150
+else:
+    model_name = 'models_model_V1_DPoint_120K_NCLass_50.h5'
+    num_class = 50
 
 file_data = st.file_uploader("Select an image", type=["jpg", "jpeg", "png"])
 
@@ -93,16 +113,31 @@ if file_data is not None:
         temp_image = str(int(time.time())) + "_" + 'img.jpg'
         img.save(temp_image)
 
-        multipart_form_data = { 
+        multipart_form_data = {
             "inputImage" : (open(temp_image, "rb"))
         }
 
-        response = requests.post(url, files = multipart_form_data, verify=False)
+        form_data = {
+            "modelName" : model_name,
+            "numClass" : num_class
+        }
+        
+        response = requests.post(url, data = form_data, files = multipart_form_data, verify=False)
 
         if os.path.exists(temp_image):
             os.remove(temp_image)
 
         prediction = response.json()['prediction']
-
-        for key, value in prediction.items():
-            st.write(f'{key}: {value} %')
+        values = prediction.values()
+        keys = prediction.keys()
+        keys = [key for key in keys]
+        values = [int(val)/100 for val in values]
+        
+        plt.figure(figsize = (8, 8))
+        plt.pie(values, labels = keys,
+                colors = ['red', 'green', 'yellow','blue','orange'],
+                pctdistance = 0.7, labeldistance = 1.4,
+                shadow = False, normalize=False)
+        plt.legend()
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
