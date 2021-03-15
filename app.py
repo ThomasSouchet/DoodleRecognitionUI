77b736f552f04f1a9ef7e5c6da51fb8528472ac0
Thomas Souchet <thomas.souchet@gmail.com>
2021-03-15 10:27:50 +0100

    Add new models and modify prediction display

--- app.py
+++ app.py
@@ -12,22 +12,22 @@ from PIL import Image, ImageDraw, ExifTags
 
 st.set_page_config(layout="wide")
 
-def load_image(path):
-    with open(path, 'rb') as f:
-        data = f.read()
-        encoded = base64.b64encode(data).decode()
-    return encoded
+# def load_image(path):
+#     with open(path, 'rb') as f:
+#         data = f.read()
+#         encoded = base64.b64encode(data).decode()
+#     return encoded
 
-encoded = load_image('images/preview.jpg')
+# encoded = load_image('images/preview.jpg')
 
-page_bg_img = f'''
-            <style>
-                body {{
-                    background-image: url("data:image/jpeg;base64,{encoded}");
-                    background-size: cover;
-                }}
-            </style>
-            '''
+# page_bg_img = f'''
+#             <style>
+#                 body {{
+#                     background-image: url("data:image/jpeg;base64,{encoded}");
+#                     background-size: cover;
+#                 }}
+#             </style>
+#             '''
 
 #st.markdown(page_bg_img, unsafe_allow_html=True)
 
@@ -36,7 +36,8 @@ st.write("# Doodle Recognition: Let’s play Pictionary !")
 st.text("")
 
 # enter here the address of your api
-url = 'https://doodle-recognition-api-x4dwwwxida-ew.a.run.app/predict/'
+#url = 'https://doodle-recognition-api-x4dwwwxida-ew.a.run.app/predict/'
+url = 'http://127.0.0.1:8000/predict'
 
 st.write("Real-time Doodle Recognition with Quick, Draw !")
 with st.beta_expander("Click here for more info about the model"):
@@ -50,22 +51,25 @@ with st.beta_expander("Click here for more info about the model"):
 
 st.text("")
 
-model = st.radio('Select a model', ('20K_NCLass_345', '25K_NCLass_190', '45K_NCLass_150', '120K_NCLass_50'))
+model = st.radio('Select a model', ('25K_NCLass_190', '45K_NCLass_150', '120K_NCLass_50', '60K_NCLass_345', '100K_NCLass_100'))
 
 st.write(model)
 
 model_name = ''
 num_class = 0
 
-if model == '20K_NCLass_345':
-    model_name = 'models_model_V1_DPoint_20K_NCLass_345.h5'
-    num_class = 345
-elif model == '25K_NCLass_190':
+if model == '25K_NCLass_190':
     model_name = 'models_model_V1_DPoint_25K_NCLass_190.h5'
     num_class = 190
 elif model == '45K_NCLass_150':
     model_name = 'models_model_V1_DPoint_45K_NCLass_150.h5'
     num_class = 150
+elif model == '100K_NCLass_100':
+    model_name = 'models_model_V1_DPoint_100K_NCLass_100.h5'
+    num_class = 100
+elif model == '60K_NCLass_345':
+    model_name = 'models_model_V1_DPoint_60K_NCLass_345.h5'
+    num_class = 345
 else:
     model_name = 'models_model_V1_DPoint_120K_NCLass_50.h5'
     num_class = 50
@@ -74,43 +78,13 @@ file_data = st.file_uploader("Select an image", type=["jpg", "jpeg", "png"])
 
 st.text("")
 
-def fix_rotation(file_data):
-    # check EXIF data to see if has rotation data from iOS. If so, fix it.
-    try:
-        image = Image.open(file_data)
-        image = image.convert("RGB")
-        for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation]=='Orientation':
-                break
-
-        exif = dict(image.getexif().items())
-
-        rot = 0
-        if exif[orientation] == 3:
-            rot = 180
-        elif exif[orientation] == 6:
-            rot = 270
-        elif exif[orientation] == 8:
-            rot = 90
-
-        if rot != 0:
-            st.write(f"Rotating image {rot} degrees (you're probably on iOS)...")
-            image = image.rotate(rot, expand=True)
-
-    except (AttributeError, KeyError, IndexError):
-        pass  # image didn't have EXIF data
-
-    return image
-
 if file_data is not None:
     with st.spinner('Checking...'):
         # load the image from uploader; fix rotation for iOS devices if necessary
         
-        img = fix_rotation(file_data)
-        
-        st.image(img)
-        
-        temp_image = str(int(time.time())) + "_" + 'img.jpg'
+        img = Image.open(file_data)
+                
+        temp_image = str(int(time.time())) + "_" + 'img.png'
         img.save(temp_image)
 
         multipart_form_data = {
@@ -132,12 +106,18 @@ if file_data is not None:
         keys = prediction.keys()
         keys = [key for key in keys]
         values = [int(val)/100 for val in values]
-        
-        plt.figure(figsize = (8, 8))
-        plt.pie(values, labels = keys,
-                colors = ['red', 'green', 'yellow','blue','orange'],
-                pctdistance = 0.7, labeldistance = 1.4,
-                shadow = False, normalize=False)
-        plt.legend()
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        st.pyplot()
+
+        c1, c2 = st.beta_columns(2)
+
+        with c1:
+            st.image(img)
+
+        with c2:     
+            plt.figure(figsize = (4, 4))
+            plt.pie(values, labels = keys,
+                    colors = ['blue','orange','red', 'green', 'yellow'],
+                    pctdistance = 0.7, labeldistance = 1.4,
+                    shadow = False, normalize=False)
+            plt.legend()
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot()
