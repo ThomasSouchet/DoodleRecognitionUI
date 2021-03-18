ae6ad3b3826a3e7f537c7d60777aa6dcdd63811d
Thomas Souchet <thomas.souchet@gmail.com>
2021-03-18 10:03:44 +0100

    Fix issue in app.py

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import streamlit as st
 # Local Imports
 import canvas
 import uploadpic
-​
+
 st.set_page_config(
     layout="wide",
     page_title = "Sketching with DL",
     page_icon = ":fire:",
     initial_sidebar_state="expanded",
     )
-​
+
 # Sidebar Navigation
 st.sidebar.title('Sketching with DL')
 options = st.sidebar.selectbox(
     "How would you like to play ?",
     ("Upload a file", "Draw something"))
-​
+
 if options == 'Upload a file':
     uploadpic.uploadpic()
 elif options == 'Draw something':
