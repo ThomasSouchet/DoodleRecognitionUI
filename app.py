cbc96fd5675f8710b7bc5eed92b2db8fe675958e
Thomas Souchet <thomas.souchet@gmail.com>
2021-03-18 09:59:23 +0100

    Remove Home page

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ import streamlit as st
 # Local Imports
 import canvas
 import uploadpic
-import home
-
+​
 st.set_page_config(
     layout="wide",
     page_title = "Sketching with DL",
     page_icon = ":fire:",
     initial_sidebar_state="expanded",
     )
-
+​
 # Sidebar Navigation
 st.sidebar.title('Sketching with DL')
 options = st.sidebar.selectbox(
     "How would you like to play ?",
-    ("Homepage", "Upload a file", "Draw something"))
-
-if options == 'Homepage':
-    home.home()
-elif options == 'Upload a file':
+    ("Upload a file", "Draw something"))
+​
+if options == 'Upload a file':
     uploadpic.uploadpic()
 elif options == 'Draw something':
     canvas.canvas()
--- home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from util import ImageUtils
-
-def home():
-    st.image(ImageUtils.get_title_img())
